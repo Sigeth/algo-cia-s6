a0069589ae343270da1638559b7ce8454a9f2d98
Lulu-rep <reppl.luc@gmail.com>
2024-03-12 10:05:13 +0100

    feat : menu V1
    First version of the display menu

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "types.h"
+#include "chainage.h"
+#include "sauvegarde.h"
+#include "menu.h"
+
 
 int main() {
-    printf("Hello, World!\n");
+    menu();
     return 0;
 }
