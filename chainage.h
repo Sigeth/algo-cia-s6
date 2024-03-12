8683a7da0d43272a8605eda1069d142fb427dfa6
Robinorum <118909581+Robinorum@users.noreply.github.com>
2024-03-12 21:50:47 +0000

    j'ai pas fini mais y a deja la fonction de chainage arriere, la chainage avant est plus compliquée et elle arrive surement demain

--- chainage.h
+++ chainage.h
@@ -1,4 +1,32 @@
 #ifndef ALGO_CIA_S6_CHAINAGE_H
 #define ALGO_CIA_S6_CHAINAGE_H
 
+// Structure pour les conditions VA AVEC RULES
+typedef struct conditions {
+    char **conditions;
+    struct conditions *suiv;
+} CONDITIONS;
+
+// Structure pour les faits INDEPENDANTS DE RULES
+typedef struct faits {
+    char **faits;
+    struct faits *suiv;
+} FAITS;
+
+// Structure pour les regles QUI INTEGRE CONDITIONS
+typedef struct rules {
+    char *conclusion;
+    CONDITIONS *ptete_conditions;
+    struct rules *suiv;
+} RULES;
+
+
+
+// Fonction pour vérifier si une règle a la conclusion donnée
+bool conclusion_est(RULES *regle, char *but);
+
+// Fonction récursive pour le chaînage arrière
+bool chainage_arriere(char *But, RULES *Base_de_regles, FAITS *Base_de_faits);
+
+
 #endif
