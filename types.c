dc82944f6e3d7218de82573450abc25f9c81cf00
Alexandre Martinez <alexandre.martinez@isen.yncrea.fr>
2024-03-12 19:47:00 +0100

    Normalement y'a tout. N'hésitez pas a signaler des problèmes s'il y a le moindre soucis

--- types.c
+++ types.c
@@ -1,1 +1,203 @@
 #include "types.h"
+
+               // CONDITIONS //
+               // init memory leakproof
+int init_Conditions(CONDITIONS ** C, int conditions_size){
+   *C = (CONDITIONS *)malloc(sizeof(CONDITIONS));
+   if (*C == NULL) {
+       printf("Erreur d'allocation de la condition\n");
+       return 0;
+   } else {
+       (*C)->conditions = malloc(conditions_size * sizeof(char *));
+       if ((*C)->conditions == NULL) {
+           printf("Erreur d'allocation des conditions\n");
+           free(*C);
+           return 0;
+       }
+       (*C)->suiv = NULL;
+       return 1;
+   }
+}
+
+int isConditionEmpty(CONDITIONS * C){
+    return (C == NULL || C->conditions == NULL);
+}
+
+int ins_Condition(CONDITIONS **TC, CONDITIONS *C){
+    if(C==NULL) {
+        printf("rien a ajouter\n");
+        return 0;
+    }
+    C->suiv=*TC;
+    *TC=C;
+    return 1;
+}
+
+int free_ListeConditions(CONDITIONS * TC) {
+    if (TC == NULL) {
+        printf("La liste de conditions est vide\n");
+        return 0;
+    }
+    free_ListeConditions(TC->suiv);
+    free(TC);
+    return 1;
+}
+
+void affiche_Condition(CONDITIONS * C) {
+    if (C == NULL) {
+        printf("Condition vide\n");
+        return;
+    }
+    for (int i = 0; C->conditions[i] != NULL; i++) {
+        printf("%s ", C->conditions[i]);
+    }
+    printf("\n");
+}
+
+int affiche_ListeConditions(CONDITIONS * TC) {
+    int count = 0;
+    if (TC != NULL) {
+        affiche_ListeConditions(TC);
+        count++;
+        count += affiche_ListeConditions(TC->suiv);
+    }
+    return count;
+}
+
+                // RULES //
+
+int init_Rule(RULES ** R, int conclusion_size){
+    *R = (RULES *)malloc(sizeof(RULES));
+    if(*R == NULL){
+        printf("Erreur d'allocation de la règle\n");
+        free(R);
+        return 0;
+    }else {
+        (*R)->conclusion = malloc((conclusion_size + 1) * sizeof(char));
+        if ((*R)->conclusion == NULL) {
+            printf("Erreur d'allocation de la conclusion\n");
+            free((*R)->conclusion);
+            free(R);
+            return 0;
+        }
+        (*R)->suiv = NULL;
+        return 1;
+    }
+}
+
+int isRuleEmpty(RULES * R) {
+    return (R == NULL || !strlen(R->conclusion));
+}
+
+int ins_Rule(RULES ** TR,RULES * R){
+    if (R == NULL){
+        printf("rien a ajouter\n");
+        return 0;}
+    if (*TR == NULL || strcmp((*TR)->conclusion, R->conclusion) >= 0) {
+        R->suiv = *TR;
+        *TR = R;
+        return 1;
+    }
+    return ins_Rule(&((*TR)->suiv), R);
+}
+
+int free_RuleList(RULES *TR){
+    if (TR == NULL) {
+        printf("Rien a libéré\n");
+        return 0;
+    }
+    free_RuleList(TR->suiv);
+    free(TR->conclusion);
+    free_ListeConditions(TR->ptete_conditions);
+    free(TR);
+    return 1;
+}
+
+void affiche_Rule(RULES * R){
+    if (R == NULL) {
+        printf("La règle est vide\n");
+        return;
+    }
+    printf("Conclusion: %s\n", R->conclusion);
+    printf("Conditions:\n");
+    affiche_ListeConditions(R->ptete_conditions);
+}
+
+int affiche_RuleList(RULES * TR){
+    int count = 0;
+    if (TR == NULL) {
+        printf("RuleList vide");
+        return 0;
+    }
+        affiche_Rule(TR);
+        count++;
+        count += affiche_RuleList(TR->suiv);
+    return count;
+}
+
+                        // FAITS //
+
+int init_Fait(FAITS ** F, int faits_size){
+    *F = (FAITS *)malloc(sizeof(FAITS ));
+    if (*F == NULL) {
+        printf("Erreur d'allocation de la condition\n");
+        return 0;
+    } else {
+        (*F)->faits = malloc(faits_size * sizeof(char *));
+        if ((*F)->faits == NULL) {
+            printf("Erreur d'allocation des conditions\n");
+            free(*F);
+            return 0;
+        }
+        (*F)->suiv = NULL;
+        return 1;
+    }
+}
+
+int isFaitEmpty(FAITS * F){
+    return (F == NULL || F->faits == NULL);
+}
+
+int ins_Fait(FAITS ** TF,FAITS * F){
+    if(F==NULL) {
+        printf("rien a ajouter\n");
+        return 0;
+    }
+    F->suiv=*TF;
+    *TF=F;
+    return 1;
+}
+
+int free_FaitList(FAITS *TF){
+    if (TF == NULL) {
+        printf("La liste de conditions est vide\n");
+        return 0; // Rien à libérer, considéré comme un succès
+    }
+    for (int i = 0; TF->faits[i] != NULL; i++) {
+        free(TF->faits[i]);
+    }
+    free_ListeConditions(TF->suiv);
+    free(TF);
+    return 1;
+}// Retourne 1 ou 0
+
+void affiche_Fait(FAITS * F){
+    if (F == NULL) {
+        printf("Fait vide\n");
+        return;
+    }
+    for (int i = 0; F->faits[i] != NULL; i++) {
+        printf("%s ", F->faits[i]);
+    }
+    printf("\n");
+}
+
+int affiche_FaitList(FAITS * TF){
+    int count = 0;
+    if (TF != NULL) {
+        affiche_ListeConditions(TF);
+        count++;
+        count += affiche_ListeConditions(TF->suiv);
+    }
+    return count;
+}
--- types.h
+++ types.h
@@ -1,4 +1,69 @@
 #ifndef ALGO_CIA_S6_TYPES_H
 #define ALGO_CIA_S6_TYPES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Conditions
+typedef struct conditions{
+    char ** conditions;
+    struct conditions * suiv;
+} CONDITIONS;
+
+//Règles
+typedef struct rules{
+    char * conclusion;
+    CONDITIONS * ptete_conditions ;
+    struct rules * suiv;
+} RULES;
+
+//Faits
+typedef struct faits{
+    char ** faits;
+    struct faits * suiv;
+}FAITS;
+
+
+//Les fonctions suivantes retourne 1 ou 0 pour être comprise par un "if" si nécessaire (1 True, 0 False)
+
+                // CONDITIONS //
+int init_Conditions(CONDITIONS ** C, int conditions_size);
+
+int isConditionEmpty(CONDITIONS * C);
+
+int ins_Condition(CONDITIONS **TC, CONDITIONS *C);
+
+int free_ListeConditions(CONDITIONS *TC);
+
+void affiche_Condition(CONDITIONS *C);
+
+int affiche_ListeConditions(CONDITIONS *TC);
+
+               // RULES //
+int init_Rule(RULES ** R, int conclusion_size);// Retourne 1 ou 0
+
+int isRuleEmpty(RULES * R);// Retourne 1 ou 0
+
+int ins_Rule(RULES ** TR,RULES * R);// Insertion par ordre alphabétique des conclusions. Retourne 1 ou 0
+
+int free_RuleList(RULES *T);// Retourne 1 ou 0
+
+void affiche_Rule(RULES * R);
+
+int affiche_RuleList(RULES * TR); // Retourne le nombre de règles en plus de les afficher
+
+               // FAITS //
+
+int init_Fait(FAITS ** F, int faits_size);// Retourne 1 ou 0
+
+int isFaitEmpty(FAITS * F);// Retourne 1 ou 0
+
+int ins_Fait(FAITS ** TF,FAITS * F);// Insertion par ordre alphabétique des conclusions. Retourne 1 ou 0
+
+int free_FaitList(FAITS *TF);// Retourne 1 ou 0
+
+void affiche_Fait(FAITS * F);
+
+int affiche_FaitList(FAITS * TF); // Retourne le nombre de règles en plus de les afficher
 #endif
