efb2a7d82b34b495f2d65f8c2bc666e575d0b6b0
Robinorum <118909581+Robinorum@users.noreply.github.com>
2024-03-16 12:38:43 +0100

    Update chainage.h

--- chainage.h
+++ chainage.h
@@ -2,33 +2,15 @@
 #define ALGO_CIA_S6_CHAINAGE_H
 
 #include <stdbool.h>
-
-
-typedef struct faits {
-    char *fait;
-    struct faits *suiv;
-} FAITS;
-
-typedef struct conditions {
-    char *condition;
-    struct conditions *suiv;
-} CONDITIONS;
-
-typedef struct rules {
-    char *conclusion;
-    CONDITIONS *ptete_conditions;
-    struct rules *suiv;
-} RULES;
-
-
+#include "types.h"
 
 
 
 
 // Prototypes des fonctions CHAINAGE AVANT
-void chainage_avant(RULES *base_de_regles, FAITS *base_de_faits);
+FAITS* chainage_avant(RULES *base_de_regles, FAITS *base_de_faits);
 bool regle_applicable(RULES *regle, FAITS *base_de_faits);
-void ajouter_fait(FAITS *base_de_faits, char *fait);
+void ajouter_fait_chainage(FAITS *base_de_faits, char *fait);
 
 
 
