49231aba630fbadb19d7cbe52b21f7e8ac0ad868
Alexandre Martinez <alexandre.martinez@isen.yncrea.fr>
2024-03-17 19:21:11 +0100

    .

--- types.c
+++ types.c
@@ -1,7 +1,7 @@
 #include "types.h"
 #include "chainage.h"
 
-// CONDITIONS //
+// CONDITIONS /
 // init memory leakproof normalement
 int init_Conditions(CONDITIONS ** C, int conditions_size){
     *C = (CONDITIONS *)malloc(sizeof(CONDITIONS));
