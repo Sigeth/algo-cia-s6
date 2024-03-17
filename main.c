218dbdeefb97229c352b8e27bdb0f196c4ad1a18
Alexandre Martinez <alexandre.martinez@isen.yncrea.fr>
2024-03-17 19:24:04 +0100

    .

--- main.c
+++ main.c
@@ -11,16 +11,26 @@
 #define HauteurFenetre 600
 
 int main(int argc, char **argv) {
-    initialiseGfx(argc, argv);
-
-    prepareFenetreGraphique("CIA_Expert_System", LargeurFenetre, HauteurFenetre);
-
-    lanceBoucleEvenements();
+    int choix=0;
+    printf("- 1 Interface graphique.\n- 2 Terminale.\n -3 Quitter");
+    scanf("%d",&choix);
+    do{
+        switch(choix){
+            case 1:
+                initialiseGfx(argc, argv);
+                prepareFenetreGraphique("CIA_Expert_System", LargeurFenetre, HauteurFenetre);
+                lanceBoucleEvenements();
+                break;
+            case 2:
+                RULES *listRules = NULL;
+                menu(listRules);
+                break;
+            case 3:
+                break;
+            default:
+                printf("Choix invalide\n");
+                break;
+        }
+    }while(choix !=3);
     return 0;
 }
-
-/*int main() {
-    RULES *listRules = NULL;
-    menu(listRules);
-    return 0;
-}*/
