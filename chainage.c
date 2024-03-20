7dc8bf9fafbe7f8e333e0d8d4d6b29cd47214586
Lulu-rep <reppl.luc@gmail.com>
2024-03-20 19:07:02 +0100

    Fix : Erreur lors du merge
    Résolu une erreur réalisé lors du merge final, non impactante pour le rendu du projet

--- chainage.c
+++ chainage.c
@@ -31,25 +31,6 @@ void ajouter_fait_chainage(FAITS *base_de_faits, char *fait) {
     base_de_faits->suiv->suiv = NULL;
 }
 
-char *trim(char *str) {
-    char *end;
-
-    // Trim leading space
-    while(isspace((unsigned char)*str)) str++;
-
-    if(*str == 0)  // All spaces?
-        return str;
-
-    // Trim trailing space
-    end = str + strlen(str) - 1;
-    while(end > str && isspace((unsigned char)*end)) end--;
-
-    // Write new null terminator character
-    end[1] = '\0';
-
-    return str;
-}
-
 
 char *trim(char *str) {
     char *end;
